f960805651ac6404965e12c101eaf33e15bd20f2
diewland <diewland@gmail.com>
2025-02-26 13:16:34 +0700

    add pfp assets

---
+++ docs/config.js
@@ -0,0 +1,19 @@
+// chain
+const CHAIN_NAME      = "Monad Testnet";
+const CHAIN_RPC       = "https://testnet-rpc.monad.xyz";
+const CHAIN_ID        = 10143;
+const CHAIN_SYMBOL    = "MON";
+const CHAIN_EXPLORER  = "https://testnet.monadexplorer.com/";
+
+// contract
+const CONTRACT_ADDR   = "0x30504Bd114e2fdf5E73f0f2705425A1b1cf40e12";
+const MINT_GAS_RATE   = 1; // TODO remove ?
+
+// player
+const PFP_EVO = {
+  10:   "./images/pociverse_261.gif",
+  100:  "./images/pociverse_1701.gif",
+  250:  "./images/pociverse_1154.gif",
+  500:  "./images/pociverse_1627.gif",
+  1000: "./images/pociverse_1551.gif",
+}
